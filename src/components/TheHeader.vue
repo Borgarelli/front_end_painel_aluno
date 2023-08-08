edc5f30f6bbbc74659a9bd0d1342518e425800ff
Kauã Borgarelli <borgarellitavares@outlook.com>
2023-08-08 17:28:09 -0300

    header fix

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,7 @@
 <template>
     <el-header class="custom-header">
       <div class="logo">Desempenho Acadêmico</div>
+      <img class="burguer" alt="menu" src="@/assets/burguer.png">
     </el-header>
   </template>
   
@@ -13,18 +14,29 @@
   
   <style scoped>
   
-  .custom-header {
-    width: 390px;
-    height: 69px;
-    flex-shrink: 0;
+  .custom-header {    
+    width: 100%;
+    height: 60px;
+    background: #fff;
+    border-radius: 0 0 12px 12px;
+    border: 2px solid var(--gray-30, #E8ECEF);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
   
   .logo {
-    font-size: 24px;
-    font-weight: bold;
-    margin-right: 20px;
-    line-height: 60px;
+    color: var(--secondary-lead, #434E5B);
+    margin: 12px;
+    font-size: 26px;color: var(--secondary-lead, #434E5B);
+    margin: 12px;
+    font-size: 26px;
   }
   
+  .burguer{
+    color: var(--secondary-lead, #434E5B);
+    margin: 12px;
+    font-size: 26px;
+  }
   </style>
   

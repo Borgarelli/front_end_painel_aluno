01c467fa42317c25067495465091fc4900d45046
Leonardi <victorleonardi.reis@layers.education>
2023-08-09 10:48:26 -0300

    Fix painel Section props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import PainelSection from  './components/PainelSection.vue'
 </script>
 
 <template>
   <div class="app">
-
-    <PainelSection label="teste" sublabel="assunto do testes">
-      <router-link to="/highlights">Adicionar a tela de seleção de alunos aqui</router-link>
-    </PainelSection>
     <RouterView/>
   </div>
 </template>
--- src/components/PainelSection.vue
+++ src/components/PainelSection.vue
@@ -14,7 +14,7 @@
 
 <script setup lang="ts">
 defineProps<{
-  label: string
+  label?: string
   sublabel?:string
 }>()
 

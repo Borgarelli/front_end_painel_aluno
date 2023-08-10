e7dbd3f95a17a568df395b255c5a75623d2580fe
pedrohlapa <141662827+pedrohlapa@users.noreply.github.com>
2023-08-10 11:35:53 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,79 @@
   </div>
   <TheFooter/>
 </template>
+ <main class="main1">
+        <div class="header-main1">
+            <div class="header-img-main1">
+                <img src="img/Component 1.png" alt="">
+            </div>
+            <div class="text-header-main1">
+                <h3>LIA</h3>
+                <p class="assistent">Assistente pedagógica virtual</p>
+            </div> 
+        </div>
+        <div class="text-main1">
+            <p>Olá responsável do Paulo :&#41;</p>
+            <p>Gostaria de compartilhar um resumo do desempenho do seu filho até esse momento do bimestre.</p>
+            <p>Tivemos 1 registro de esquecimento de material e um comparecimento de apenas 65%. Além disso, o seu desempenho na avaliação do período foi de 56% do total.</p>
+            <p>Sabemos que esses resultados podem sempre serem melhorados. Por isso, acompanhe os dados detalhadamente a seguir.</p>
+        </div>
+    </main>
 
+main{
+    width: 90%;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    margin: 0 5% 0 5%;
+    border-radius: 17px;
+    margin-bottom: 25px;
+}
+
+.main1{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 330px;
+}
+
+.header-main1{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 14px;
+    width: 100%;
+    margin-top: -15px;
+}
+
+
+.text-header-main1{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    margin-left: 12px;  
+}
+
+h3{
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 800;
+}
+
+.text-header-main1 > p{
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    color: var(--gray-70, #748494);
+}
+
+.text-main1{
+    margin-top: 10px;
+    width: 90%;
+    font-size: 15px;
+    color: var(--secondary-lead, #434E5B);
+    font-weight: 600;
+    text-align: justify; 
+}
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'

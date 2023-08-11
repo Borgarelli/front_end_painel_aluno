fceba70845cbddc3228909908fb435ab5609f9f8
Leonardi <victorleonardi.reis@layers.education>
2023-08-11 02:12:08 -0300

    Add assets

--- src/views/StudentPanel.vue
+++ src/views/StudentPanel.vue
@@ -8,13 +8,12 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { getTerms, getTermsName } from '../services/studentPanel.ts'
+import { getTermsName, getStatusPerSubject } from '../services/studentPanel.ts'
 import AppBar  from '../components/AppBar.vue';
 import TermsBar from '../components/TermsBar.vue';
 
 const termsNames = ref(getTermsName())
-const terms = getTerms()
-console.log(terms)
+console.log(getStatusPerSubject())
 
 </script>
 
